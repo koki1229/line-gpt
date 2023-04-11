cadc510f87e71e540221b0f629fd6f9091fe608c
koki1229 <katsumotokoki@MacBook-Air.local>
2023-04-11 22:20:05 +0900

    change

--- app.py
+++ app.py
@@ -48,5 +48,5 @@ def handle_message(event):
         TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
